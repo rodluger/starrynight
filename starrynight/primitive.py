sign errors fixed

starrynight/primitive.py
@@ -382,7 +382,7 @@ def compute_Q(ydeg, lam):
     return Q
 
 
-def compute_T(ydeg, b, theta, xi):
+def compute_T(ydeg, b, theta, xi, tol=1e-9):
 
     # Pre-compute H
     H = compute_H(ydeg + 2, xi)
@@ -401,6 +401,86 @@ def compute_T(ydeg, b, theta, xi):
 
     # Case 2 (special)
     T[2] = pairdiff([np.sign(b) * _compute_T2_indef(np.abs(b), x) for x in xi])
+
+    # Special limits: theta = 0, pi/2, pi, 3pi/2
+    if np.abs(st) < tol:
+
+        n = 0
+        for l in range(ydeg + 1):
+            for m in range(-l, l + 1):
+                mu = l - m
+                nu = l + m
+                if nu % 2 == 0:
+                    T[n] = (
+                        np.sign(ct) ** l
+                        * b ** (1 + nu // 2)
+                        * H[(mu + 4) // 2, nu // 2]
+                    )
+                else:
+                    if mu == 1:
+                        if (l % 2) == 0:
+                            T[n] = -np.sign(ct) * b32 * H[l - 2, 4]
+                        elif l > 1:
+                            T[n] = -b * b32 * H[l - 3, 5]
+                    else:
+                        T[n] = np.sign(ct) ** (l - 1) * (
+                            b32 * b ** ((nu + 1) // 2) * H[(mu - 1) // 2, (nu + 5) // 2]
+                        )
+                n += 1
+
+        return T
+
+    elif np.abs(ct) < tol:
+
+        n = 0
+        for l in range(ydeg + 1):
+            for m in range(-l, l + 1):
+                mu = l - m
+                nu = l + m
+                if nu % 2 == 0:
+                    T[n] = (
+                        np.sign(ct) ** l
+                        * b ** ((mu + 2) // 2)
+                        * H[nu // 2, (mu + 4) // 2]
+                    )
+
+                    if np.sign(st) == 1:
+                        T[n] *= (-1) ** (mu // 2)
+                    else:
+                        T[n] *= (-1) ** (nu // 2)
+
+                else:
+                    if mu == 1:
+                        if (l % 2) == 0:
+
+                            T[n] = (
+                                (-np.sign(st)) ** (l - 1)
+                                * b ** (l - 1)
+                                * b32
+                                * H[1, l + 1]
+                            )
+
+                        elif l > 1:
+
+                            T[n] = b ** (l - 2) * b32 * H[2, l]
+                            if np.sign(st) == 1:
+                                T[n] *= (-1) ** l
+                            else:
+                                T[n] *= -1
+                    else:
+
+                        T[n] = (
+                            b32 * b ** ((mu - 3) // 2) * H[(nu - 1) // 2, (mu + 5) // 2]
+                        )
+
+                        if np.sign(st) == 1:
+                            T[n] *= -((-1) ** ((mu - 3) // 2))
+                        else:
+                            T[n] *= (-1) ** ((nu - 1) // 2)
+
+                n += 1
+
+        return T
 
     # Cases 1 and 5
     jmax = 0

starrynight/viz.py
@@ -8,8 +8,8 @@ from matplotlib.patches import Arc
 def visualize(b, theta, bo, ro, tol=1e-7, res=4999):
 
     # Find angles of intersection
-    phi, lam, xi, _ = get_angles(b, theta, bo, ro, tol=tol)
-    phi = phi.reshape(-1, 2)
+    kappa, lam, xi, _ = get_angles(b, theta, bo, ro, tol=tol)
+    phi = (kappa - np.pi / 2).reshape(-1, 2)
     lam = lam.reshape(-1, 2)
     xi = xi.reshape(-1, 2)
 

tests/test_analytic.py
@@ -1,15 +1,14 @@
+from utils import compare
 from cases import CASE
 from numerical import Numerical
 from starrynight import StarryNight
 import numpy as np
 import pytest
 
-
+# Params
 ydeg = 5
 S = StarryNight(ydeg, tol=1e-7)
 N = Numerical(ydeg, tol=1e-7)
-
-
 args = CASE[1] + CASE[2] + CASE[3] + CASE[4] + CASE[5]
 
 
@@ -19,7 +18,7 @@ args = CASE[1] + CASE[2] + CASE[3] + CASE[4] + CASE[5]
 def test_P(b, theta, bo, ro):
     N.precompute(b, theta, bo, ro)
     S.precompute(b, theta, bo, ro)
-    assert np.allclose(N.P, S.P)
+    compare(N.P, S.P)
 
 
 @pytest.mark.parametrize(
@@ -28,7 +27,7 @@ def test_P(b, theta, bo, ro):
 def test_T(b, theta, bo, ro):
     N.precompute(b, theta, bo, ro)
     S.precompute(b, theta, bo, ro)
-    assert np.allclose(N.T, S.T)
+    compare(N.T, S.T)
 
 
 @pytest.mark.parametrize(
@@ -37,4 +36,4 @@ def test_T(b, theta, bo, ro):
 def test_Q(b, theta, bo, ro):
     N.precompute(b, theta, bo, ro)
     S.precompute(b, theta, bo, ro)
-    assert np.allclose(N.Q, S.Q)
+    compare(N.Q, S.Q)